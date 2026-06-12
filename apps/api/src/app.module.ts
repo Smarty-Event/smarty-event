8dc7deca0fe7d157a78fdb53306fea15fb906bd8
ralphbetta <ralphbetta@gmail.com>
2026-06-12 17:44:43 +0100

    feat(api): connect all feature modules and database client to AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TenantModule } from "./tenant/tenant.module";
+import { EventModule } from "./event/event.module";
+import { TicketModule } from "./ticket/ticket.module";
+import { CheckInModule } from "./checkin/checkin.module";
 
 @Module({
-  imports: [],
+  imports: [TenantModule, EventModule, TicketModule, CheckInModule],
   controllers: [AppController],
   providers: [AppService],
 })
